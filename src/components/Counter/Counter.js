ddfbfcebfe31d17f3e811c595d045b05252d40a6
Igor_Popovich <78104120+IgorPopovich@users.noreply.github.com>
2023-02-23 13:21:41 +0200

    add

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Controls from './Controls';
+import './Counter.css';
+
+class Counter extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0
+  }
+
+  countTotalFeedback() {
+
+  }
+
+  countPositiveFeedbackPercentage() {
+
+  }
+
+  handleGood = () => {
+
+  }
+
+  handleNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }))
+  }
+
+  handleBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='counter'>
+          <p className='title'>Please leave feedback</p>
+          <Controls
+            clickGood={this.handleGood}
+            clickNeutral={this.handleNeutral}
+            clickBad={this.handleBad}
+          />
+          <p className='title'>Statistics</p>
+          <div className='result'>
+            <span className='value'>Good: {this.state.good}</span>
+            <span className='value'>Neutral: {this.state.neutral}</span>
+            <span className='value'>Bad: {this.state.bad}</span>
+            <span className='value'>Total: {this.state.total}</span>
+            <span className='value'>Positive feedback: 0%</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter;
